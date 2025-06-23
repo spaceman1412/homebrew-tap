fe137ce26d9b46236315ce2a9e5aa21d963a5b94
spaceman1412 <spacemannormal@gmail.com>
2025-06-23 10:36:27 +0700

    Update cask

--- Casks/justlockin.rb
+++ Casks/justlockin.rb
@@ -1,25 +1,15 @@
-      
-# In Casks/justlockin.rb
-
-cask "justlockin" do
-  # The version number MUST match your GitHub Release tag
-  version "1.0.0"
-  
-  # The SHA256 hash you calculated in step 1
+cask "justlockin" do 
+  version '1.0.0'
   sha256 "2ecfa32e502a37655dff21f9897edf3ae26edd6eed72bc2bb6660d050cbb8344"
 
-  # The public download URL from your GitHub Release
-  url "https://github.com/spaceman1412/justlockin/releases/download/v#{version}/JustLockin-v1.0.zip"
-  
-  # The official name of your app
+  url "https://github.com/spaceman1412/justlockin/releases/download/v#{version}/JustLockin-v#{version}.zip"
   name "JustLockin"
-  
-  # Your project's homepage
+  desc "JustLockin is a simple pomodoro app for macOS"
   homepage "https://github.com/spaceman1412/justlockin"
 
-  # This line tells Homebrew what to do after unzipping.
-  # It moves the .app file to the /Applications folder.
+  postflight do
+    system "xattr -d com.apple.quarantine #{appdir}/JustLockin.app"
+  end
+
   app "JustLockin.app"
 end
-
-    
