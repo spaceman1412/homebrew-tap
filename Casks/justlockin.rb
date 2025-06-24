51174266b3af2d9679504bfb69e1cd72f690c943
spaceman1412 <spacemannormal@gmail.com>
2025-06-24 15:54:39 +0700

    Add check version support newer macos 13

--- Casks/justlockin.rb
+++ Casks/justlockin.rb
@@ -7,6 +7,8 @@ cask "justlockin" do
   desc "JustLockin is a simple pomodoro app for macOS"
   homepage "https://github.com/spaceman1412/justlockin"
 
+  depends_on macos: ">= :ventura" # macOS 13
+
   postflight do
     system "xattr -d com.apple.quarantine #{appdir}/JustLockin.app"
   end
