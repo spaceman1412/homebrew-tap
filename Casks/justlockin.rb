fee7d79c6179317785600570ec874ddce9aa70bb
spaceman1412 <spacemannormal@gmail.com>
2025-07-01 10:59:22 +0700

    Update version 1.1.0

--- Casks/justlockin.rb
+++ Casks/justlockin.rb
@@ -1,6 +1,6 @@
 cask "justlockin" do 
-  version '1.0.1'
-  sha256 "77fb8a392618f47acacd136d0b7781aed977ab755891e3cff67c95b9fa7fac41"
+  version '1.1.0'
+  sha256 "8a3d065a90be81916ec9f0ced3b39ac9cc6515243b4bae79ba07e331b3bf7e80"
 
   url "https://github.com/spaceman1412/justlockin/releases/download/v#{version}/JustLockin-v#{version}.zip"
   name "JustLockin"
